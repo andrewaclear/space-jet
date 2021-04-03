07805e01f7c3d11c366f016ee25ff0ae38e173ea
Andrew D <51063733+andrewaclear@users.noreply.github.com>
2021-04-02 22:20:01 -0400

    base outline

---
+++ game.asm
@@ -0,0 +1,55 @@
+#####################################################################
+#
+# CSCB58 Winter 2021 Assembly Final Project
+# University of Toronto, Scarborough
+#
+# Student: Andrew D'Amario, 1006618947, damario4
+#
+# Bitmap Display Configuration:
+# - Unit width in pixels: 8 (update this as needed)
+# - Unit height in pixels: 8 (update this as needed)
+# - Display width in pixels: 256 (update this as needed)# - Display height in pixels: 256 (update this as needed)
+# - Base Address for Display: 0x10008000 ($gp)
+#
+# Which milestones have been reached in this submission?
+# (See the assignment handout for descriptions of the milestones)
+# - Milestone 1/2/3/4 (choose the one the applies)
+#
+# Which approved features have been implemented for milestone 4?
+# (See the assignment handout for the list of additional features)
+# 1. (fill in the feature, if any)
+# 2. (fill in the feature, if any)
+# 3. (fill in the feature, if any)
+# ... (add more if necessary)
+#
+# Link to video demonstration for final submission:
+# - (insert YouTube / MyMedia / other URL here). Make sure we can view it!
+#
+# Are you OK with us sharing the video with people outside course staff?
+# - yes / no / yes, and please share this project github link as well!
+#
+# Any additional information that the TA needs to know:
+# - (write here, if any)
+#
+#####################################################################
+
+# defined CONSTANTS
+.eqv
+
+
+.data
+# variables
+# use arrays for storing obstacle locations
+
+.text
+# clear screen 
+
+main:
+# Check for keyboard input and update ship location.
+# Update obstacle location.
+# Check for various collisions (e.g., between ship and obstacles).
+# Update other game state and end of game.
+# Erase objects from the old position on the screen.
+# Redraw objects in the new position on the screen.
+
+# At the end of each iteration, your main loop should sleep for a short time and go back to step 1.
