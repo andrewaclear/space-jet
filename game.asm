96459c5a4901ec54f883f669b31921da40c3f8b5
andrewaclear <andrew@thecross.ca>
2021-04-14 18:44:09 -0400

    remove personal info

--- game.asm
+++ game.asm
@@ -5,7 +5,7 @@
 # CSCB58 Winter 2021 Assembly Final Project
 # University of Toronto, Scarborough
 #
-# Student: Andrew D'Amario, 1006618947, damario4
+# Student: Andrew D'Amario
 #
 # Bitmap Display Configuration:
 # - Unit width in pixels: 8
